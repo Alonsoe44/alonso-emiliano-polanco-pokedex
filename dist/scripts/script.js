8649bcb7e565d140a8d1d7b31acd81cf7d59311c
Emiliano Polanco <emilianopolanco5@gmail.com>
2022-01-29 14:02:49 +0100

    Basic card design done

--- dist/scripts/script.js
+++ dist/scripts/script.js
@@ -1,12 +1,11 @@
-console.log("experiment");
-const emptyBox = document.querySelector(".blank");
+const emptyBox = document.querySelector(".pokemon-card__pokemon-image");
 const asyncPokemon = async () => {
   const response = await fetch("https://pokeapi.co/api/v2/pokemon/");
   const responseJson = await response.json();
   const pokeInfoResponse = await fetch(responseJson.results[0].url);
   const pokeInfo = await pokeInfoResponse.json();
 
-  const pokePictureResponse = pokeInfo.sprites.front_default;
+  const pokePictureResponse = pokeInfo.sprites.other.dream_world.front_default;
 
   emptyBox.src = pokePictureResponse;
 };
