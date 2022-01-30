4d06a8d555f21844a62029277e3591248d312f43
EmilianoIpad <emilianopolanco5@gmail.com>
2022-01-30 15:59:54 +0000

    Page component created

--- dist/scripts/components/NavegationBar.js
+++ dist/scripts/components/NavegationBar.js
@@ -3,25 +3,5 @@ import Component from "./Component.js";
 export default class NavegationBar extends Component {
   constructor(parentElement) {
     super(parentElement, "navegation-bar", "nav");
-    this.createSons();
-  }
-
-  createSons() {
-    this.element.innerHTML = `<div class="option-container">
-          <a class="navegation-bar__icon"><i class="fas fa-home"></i></a>
-          <span class="navegation-bar__description">Pokepets</span>
-        </div>
-        <div class="option-container">
-          <a><i class="fas fa-globe"></i></a>
-          <span class="navegation-bar__description">World</span>
-        </div>
-        <div class="option-container">
-          <a><i class="fas fa-chevron-right"></i></a>
-          <span class="navegation-bar__description">Return</span>
-        </div>
-        <div class="option-container">
-          <a><i class="fas fa-chevron-right"></i></a>
-          <span class="navegation-bar__description">Next</span>
-        </div>`;
   }
 }
--- dist/scripts/script.js
+++ dist/scripts/script.js
@@ -1,32 +1,4 @@
-import Card from "./components/Card.js";
-import NavegationBar from "./components/NavegationBar.js";
+import Page from "./components/Page.js";
 
 const appContainerDom = document.querySelector(".app-container");
-
-const asyncPokemon = async () => {
-  const response = await fetch("https://pokeapi.co/api/v2/pokemon/?limit=5");
-  const responseJson = await response.json();
-  const pokemonGroup = responseJson.results;
-  let pokemonGroupObjects = pokemonGroup.map((pokemon) => pokemon.name);
-  const pokemonGroupPromeses = pokemonGroup.map((pokemon) =>
-    fetch(pokemon.url)
-  );
-  const pokemonGroupStrings = await Promise.all(pokemonGroupPromeses);
-  const pokemonGroupData = await Promise.all(
-    pokemonGroupStrings.map((pokeString) => pokeString.json())
-  );
-  pokemonGroupObjects = pokemonGroupObjects.map((pokemon, index) => ({
-    name: pokemon,
-    image: pokemonGroupData[index].sprites.other.dream_world.front_default,
-    weight: pokemonGroupData[index].weight,
-    height: pokemonGroupData[index].height,
-    types: pokemonGroupData[index].types[0].type.name,
-    moves: pokemonGroupData[index].moves[0].move.name,
-  }));
-  pokemonGroupObjects.forEach(
-    (pokemon) => new Card(appContainerDom, pokemon.name, pokemon.image)
-  );
-};
-
-asyncPokemon();
-new NavegationBar(appContainerDom);
+new Page(appContainerDom, "page-container");
